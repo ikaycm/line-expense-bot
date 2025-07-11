d7d1f4596b5703a2c77fbf59e4cf4054395593b8
ikaycm <okaycmstudio@gmail.com>
2025-07-11 17:06:45 +0700

    Add package.json and express server

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express');
+const { Client } = require('@line/bot-sdk');
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+const client = new Client({
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
+});
 
 app.use(express.json());
 
@@ -9,8 +16,25 @@ app.get('/', (req, res) => {
 });
 
 app.post('/webhook', (req, res) => {
-  console.log('Received webhook:', req.body);
-  res.sendStatus(200);
+  const events = req.body.events;
+  if (!events) {
+    return res.sendStatus(400);
+  }
+
+  Promise.all(events.map(event => {
+    if (event.type === 'message' && event.message.type === 'text') {
+      return client.replyMessage(event.replyToken, {
+        type: 'text',
+        text: `คุณส่งข้อความว่า: ${event.message.text}`
+      });
+    }
+    return Promise.resolve(null);
+  }))
+  .then(() => res.sendStatus(200))
+  .catch(err => {
+    console.error(err);
+    res.sendStatus(500);
+  });
 });
 
 app.listen(PORT, () => {
