8caec5e7331313401e1516e77fb369c3cba3ca4a
ikaycm <okaycmstudio@gmail.com>
2025-07-11 16:53:22 +0700

    Add package.json and express server

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello, line-expense-bot!");
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('LINE Expense Bot is running');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
